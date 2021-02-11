3184bb04f3a669a350bcf5f97a67b31bafcdc277
gero-web <78724836+gero-web@users.noreply.github.com>
2021-02-11 15:48:46 +0800

    gurd_fix_url

--- grudapp/urls.py
+++ grudapp/urls.py
@@ -4,9 +4,9 @@ from grudapp import views
 urlpatterns =[
       
        url(r'^task/', views.task),
-       url(r'^create_task/', views.create_task),
+       url(r'^create_task', views.create_task),
        
        url(r'^update_task/(?P<pk>[0-9]+)$', views.update_task,name='update_task'),
-       url(r'^detail_task/(?P<pk>[0-9]+)$/', views.detail_task),
+       url(r'^detail_task/(?P<pk>[0-9]+)$', views.detail_task),
        url(r'^delate_task/(?P<pk>[0-9]+)$', views.delate_task,name='delate_task'),
 ]
